99e4115ed18300b88fc9cd9641183ae20de580af
dxrk-kali <dxrkanon@gmail.com>
2022-07-08 14:15:38 -0400

    sourced it for user

--- run.sh
+++ run.sh
@@ -7,7 +7,13 @@ YLLW='\033[1;33m'
 DG='\033[0;37m'
 LR='\033[0;31m'
 #END COLORS 
+if [ $EUID != 0 ]
+then
+	printf "Needs to be run as root/sudo user.\n"
+	exit 1;
+fi
 shell=`echo $SHELL`
 shtype=`echo .${shell:9}rc` 
 echo PATH=$(pwd):$PATH >> ~/$shtype
+source ~/$shtype
 
